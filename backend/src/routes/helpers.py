9dd967ead8af0d4e38acba26529b8e71d102c7bd
Alex Xu <xulex6@gmail.com>
2023-12-12 07:17:12 +0000

    feat: add generic queries

--- backend/src/routes/helpers.py
+++ backend/src/routes/helpers.py
@@ -6,13 +6,25 @@ from ..error import InputError
 from ..models.user import User
 
 
-def fetch_one(model, filter_criteria, error_message):
+def fetch_one(model, filter):
     try:
-        return db.session.execute(
-            db.select(model).filter_by(**filter_criteria)
-        ).scalar_one()
+        return db.session.execute(db.select(model).filter_by(**filter)).scalar_one()
     except NoResultFound as exc:
-        raise InputError(error_message) from exc
+        raise InputError(
+            f"ERROR: {model.__name__} {list(filter.values())[0]} was not found."
+        ) from exc
+
+
+def fetch_all(model, filter=None):
+    try:
+        if filter:
+            return (
+                db.session.execute(db.select(model).filter_by(**filter)).scalars().all()
+            )
+        else:
+            return db.session.execute(db.select(model)).scalars().all()
+    except NoResultFound as exc:
+        raise InputError(f"ERROR: {model.__name__}s not found.") from exc
 
 
 def fetch_one_by_id(model, id, error_message):
