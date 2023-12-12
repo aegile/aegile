34329a1e5a9572e730cd275be92600ba2ef608e4
Alex Xu <xulex6@gmail.com>
2023-12-12 04:19:15 +0000

    feat: add course id field and flask-restx api models

--- backend/src/models/course.py
+++ backend/src/models/course.py
@@ -1,10 +1,19 @@
-from src.extensions import db
+from flask_restx import fields
+from ..extensions import api, db
+from .user import UserSet
 
 
 class Course(db.Model):
-    # id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(8), nullable=False, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(8), nullable=False, unique=True)
     name = db.Column(db.String(100), nullable=False)
+    userset_id = db.Column(db.Integer, db.ForeignKey("user_set.id"), unique=True)
+    userset = db.relationship("UserSet", backref="courses", uselist=False)
+
+    def __init__(self, code: str, name: str):
+        self.code = code
+        self.name = name
+        self.userset = UserSet()
 
     def __repr__(self):
         return f"<Course {self.code=} {self.name=}>"
@@ -13,3 +22,12 @@ class Course(db.Model):
 class CourseOffering(db.Model):
     code = db.Column(db.String(8), db.ForeignKey("course.code"), primary_key=True)
     term = db.Column(db.String(4), primary_key=True)
+
+
+course_fetch_model = api.model(
+    "Course", {"id": fields.Integer, "code": fields.String, "name": fields.String}
+)
+
+course_new_model = api.model(
+    "NewCourseInput", {"code": fields.String, "name": fields.String}
+)
