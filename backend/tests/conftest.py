e513b722e00f4d6bba72105c707da386377c7c2b
Alex Xu <xulex6@gmail.com>
2023-12-20 12:25:38 +0000

    feat: add pytest fixture for creating a mock flask client

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,15 +1,35 @@
 import pytest
 from flask import Flask
+from src import create_app
 from src.extensions import db
+
+
+# @pytest.fixture(scope="module")
+# def test_app():
+#     app = Flask(__name__)
+#     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+#     app.config["TESTING"] = True
+#     db.init_app(app)
+#     return app
 
 
 @pytest.fixture(scope="module")
 def test_app():
-    app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
-    app.config["TESTING"] = True
-    db.init_app(app)
+    app = create_app()
+    from src.models.user import User, UserSet
+
+    app.config.update({"TESTING": True})
+    app.config.update({"SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:"})
     return app
+
+
+@pytest.fixture(scope="module")
+def client(test_app):
+    with test_app.app_context():
+        db.create_all()
+        yield test_app.test_client()
+        db.session.remove()
+        db.drop_all()
 
 
 @pytest.fixture(scope="function")
