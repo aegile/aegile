d7128c860d15f57a6189b6534844a418927512d0
Alex Xu <xulex6@gmail.com>
2023-12-26 09:01:08 +0000

    feat: rename query to check to avoid db confusion

--- backend/src/routes/access_checks.py
+++ backend/src/routes/access_checks.py
@@ -4,14 +4,25 @@ from ..models.user import User
 from ..models.role import Role
 
 
-def query_course_creator(course: Course, user: User):
+def check_course_creator(course: Course, user: User):
     if user.id != course.creator:
         raise AccessError(
             f"You are not the creator of this {(Course.__name__).lower()}"
         )
 
 
-def query_in_userset(model, object, user, is_auth_user=True):
+def check_is_member(model, object, user, is_auth_user=True):
+    if user not in object.members:
+        if is_auth_user:
+            raise AccessError(
+                f"You are not a member of this {(model.__name__).lower()}"
+            )
+        raise NotFoundError(
+            f"{user.handle} is not a member of this {(model.__name__).lower()}"
+        )
+
+
+def check_in_userset(model, object, user, is_auth_user=True):
     if user not in object.userset.members:
         if is_auth_user:
             raise AccessError(
