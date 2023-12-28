6db30c463a65acef7c7b1e1c6109b294cb081217
Alex Xu <xulex6@gmail.com>
2023-12-28 06:44:03 +0000

    feat: change use of course code as identifier to course ID due to term offerings

--- backend/src/models/group.py
+++ backend/src/models/group.py
@@ -6,22 +6,20 @@ from .helpers import get_with_default
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    course_code = db.Column(db.String(8), db.ForeignKey("course.code"), nullable=False)
+    course_id = db.Column(db.String, db.ForeignKey("course.id"), nullable=False)
     tutorial_id = db.Column(db.String, db.ForeignKey("tutorial.id"), nullable=False)
     # projects = db.relationship("Project", backref="group", cascade="all, delete-orphan")
     userset_id = db.Column(db.Integer, db.ForeignKey("user_set.id"), unique=True)
     userset = db.relationship("UserSet", backref="groups", uselist=False)
 
-    def __init__(self, course_code: str, tutorial_id: int, name: str):
-        self.course_code = course_code
+    def __init__(self, course_id: str, tutorial_id: int, name: str):
+        self.course_id = course_id
         self.tutorial_id = tutorial_id
         self.name = name
         self.userset = UserSet()
 
     def update(self, course_data: dict):
-        self.course_code = get_with_default(
-            course_data, "course_code", self.course_code
-        )
+        self.course_id = get_with_default(course_data, "course_id", self.course_id)
         self.tutorial_id = get_with_default(
             course_data, "tutorial_id", self.tutorial_id
         )
@@ -31,4 +29,4 @@ class Group(db.Model):
         self.userset.members = users
 
     def __repr__(self):
-        return f"<Group {self.id} {self.course_code}→{self.tutorial_id}→{self.name}>"
+        return f"<Group {self.id} {self.course_id}→{self.tutorial_id}→{self.name}>"
