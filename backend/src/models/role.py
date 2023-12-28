e51e3b0e2731e543631d3707428f73e915a6697f
Alex Xu <xulex6@gmail.com>
2023-12-28 04:28:46 +0000

    feat: change course code for course ID

--- backend/src/models/role.py
+++ backend/src/models/role.py
@@ -11,23 +11,31 @@ class Permission(db.Model):
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False, default="new role")
-    course_code = db.Column(db.String(8), db.ForeignKey("course.code"), nullable=False)
+    course_id = db.Column(db.String, db.ForeignKey("course.id"), nullable=False)
     color = db.Column(db.String(7), nullable=False, default="#868686")
-    can_manage_roles = db.Column(db.Integer, nullable=False, default=0)
-    can_access_tutorials = db.Column(db.Integer, nullable=False, default=0)
     user_course_statuses = db.relationship(
         "UserCourseStatus", backref="role", lazy=True
     )
 
-    def __init__(self, name: str, course_code: str):
+    can_manage_roles = db.Column(db.Integer, nullable=False, default=0)
+    can_manage_course = db.Column(db.Integer, nullable=False, default=0)
+    can_access_tutorials = db.Column(db.Integer, nullable=False, default=0)
+
+    def __init__(self, name: str, course_id: str):
         self.name = name
-        self.course_code = course_code
+        self.course_id = course_id
         # self.members = members
         # self.permissions = permissions
 
     def update(self, role_data: dict):
         self.name = get_with_default(role_data, "name", self.name)
         self.color = get_with_default(role_data, "color", self.color)
+        for perm in role_data["permissions"]:
+            self.set_permission(perm, self.has_permission(perm) ^ 1)
+
+    @property
+    def member_count(self):
+        return len(self.user_course_statuses)
 
     @property
     def members(self):
@@ -37,6 +45,7 @@ class Role(db.Model):
     def permissions(self):
         return {
             "can_access_tutorials": self.can_access_tutorials,
+            "can_manage_course": self.can_manage_course,
             "can_manage_roles": self.can_manage_roles,
         }
 
