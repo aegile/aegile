da4536aec8170d02ea3d4cc0e1d7339c93b82c00
Alex Xu <xulex6@gmail.com>
2023-12-28 06:04:00 +0000

    feat: update auth route tests to not use setup fixtures

--- backend/tests/test_routes/test_auth_routes.py
+++ backend/tests/test_routes/test_auth_routes.py
@@ -1,24 +1,41 @@
-def test_valid_registration(client):
-    form_data = {
-        "first_name": "Alex",
-        "last_name": "Xu",
-        "email": "alex@email.com",
-        "password": "AlexXu123!",
-    }
-    response = client.post("v1/auth/register", json=form_data)
-    assert response.status_code == 201
+import pytest
 
 
+@pytest.mark.xfail(reason="SQLite doesn't support character limits")
 def test_invalid_name_overflow_registration(client):
     form_data = {
         "first_name": "A" * 51,
         "last_name": "Xu",
-        "email": "alex@email.com",
+        "email": "invalidname.overflow@email.com",
         "password": "AlexXu123!",
     }
     response = client.post("v1/auth/register", json=form_data)
-    # assert response.status_code == 400
-    assert response.status_code == 201
+    assert response.status_code == 400
+
+
+@pytest.mark.xfail(reason="SQLite doesn't support character limits")
+def test_invalid_email_overflow_registration(client):
+    form_data = {
+        "first_name": "AlexAlex",
+        "last_name": "Xu",
+        "email": "AlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlex@email.com",
+        "password": "AlexXu123!",
+    }
+    response = client.post("v1/auth/register", json=form_data)
+    print(response.json)
+    assert response.status_code == 400
+
+
+@pytest.mark.xfail(reason="SQLite doesn't support character limits")
+def test_invalid_password_overflow_registration(client):
+    form_data = {
+        "first_name": "Inva",
+        "last_name": "Lid",
+        "email": "invalidpassword.overflow@email.com",
+        "password": "Alexela" * 10,
+    }
+    response = client.post("v1/auth/register", json=form_data)
+    assert response.status_code == 400
 
 
 def test_invalid_empty_email_registration(client):
@@ -43,31 +60,19 @@ def test_invalid_null_password_registration(client):
     assert response.status_code == 400
 
 
-def test_invalid_email_overflow_registration(client):
-    form_data = {
-        "first_name": "Alex",
-        "last_name": "Xu",
-        "email": "AlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlex@email.com",
-        "password": "AlexXu123!",
-    }
-    response = client.post("v1/auth/register", json=form_data)
-    # assert response.status_code == 400
-    assert response.status_code == 201
-
-
-def test_invalid_password_overflow_registration(client):
+def test_valid_registration(client):
     form_data = {
         "first_name": "Alex",
         "last_name": "Xu",
         "email": "alex@email.com",
-        "password": "Alexela" * 10,
+        "password": "AlexXu123!",
     }
     response = client.post("v1/auth/register", json=form_data)
-    # assert response.status_code == 400
+    print(response.json)
     assert response.status_code == 201
 
 
-def test_invalid_existing_email_registration(client, users_setup):
+def test_invalid_existing_email_registration(client):
     form_data = {
         "first_name": "Alex",
         "last_name": "Xu",
@@ -78,7 +83,7 @@ def test_invalid_existing_email_registration(client, users_setup):
     assert response.status_code == 400
 
 
-def test_valid_login(client, users_setup):
+def test_valid_login(client):
     form_data = {
         "email": "alex@email.com",
         "password": "AlexXu123!",
@@ -87,7 +92,7 @@ def test_valid_login(client, users_setup):
     assert response.status_code == 200
 
 
-def test_invalid_password_login(client, users_setup):
+def test_invalid_password_login(client):
     form_data = {
         "email": "alex@email.com",
         "password": "loremipsum",
@@ -96,7 +101,7 @@ def test_invalid_password_login(client, users_setup):
     assert response.status_code == 401
 
 
-def test_invalid_nonexistent_user_login(client, users_setup):
+def test_invalid_nonexistent_user_login(client):
     form_data = {"email": "nonexistent_user@email.com", "password": "password"}
     response = client.post("v1/auth/login", json=form_data)
     assert response.status_code == 400
