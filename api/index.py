bf4177a7da3a6483934895f67f460ed5ed9430c7
Alex Xu <xulex6@gmail.com>
2023-12-31 12:22:48 +0000

    feat: add flask-restx test route

--- api/index.py
+++ api/index.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_restx import Api, Resource
+
 app = Flask(__name__)
+api = Api(app)
+
+@api.route('/api/test')
+class NewYear(Resource):
+    def get(self):
+        return "<p>Happy New Year!</p>"
 
 @app.route("/api/python")
 def hello_world():
