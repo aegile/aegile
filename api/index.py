461fd54a9ef36a52f49afa9dc8f258b3a97e6a31
Alex Xu <xulex6@gmail.com>
2024-01-01 09:07:28 +0000

    feat: add backend entry point in api index file

--- api/index.py
+++ api/index.py
@@ -1,6 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+from backend.src.extensions import db
+from backend.src import create_app
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app = create_app()
+with app.app_context():
+    db.drop_all()
+    print(">>> creating tables")
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
--- backend/src/app.py
+++ backend/src/app.py
@@ -1,11 +1,11 @@
 from src.extensions import db
 from src import create_app
 
+app = create_app()
+with app.app_context():
+    db.drop_all()
+    print(">>> creating tables")
+    db.create_all()
 
 if __name__ == "__main__":
-    app = create_app()
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-
     app.run(debug=True, port=5000)
