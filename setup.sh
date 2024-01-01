9a1ee5b68c25848fb09a572edcf4d4466a17d4e9
Alex Xu <xulex6@gmail.com>
2024-01-01 10:09:05 +0000

    feat: update local dev environment setup

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ sudo npm install
 sudo apt-get update	sudo apt-get update
 sudo apt install python3-pip -y	sudo apt install python3-pip -y
 sudo apt install python3-venv -y	sudo apt install python3-venv -y
+python3 -m venv .venv && source .venv/bin/activate
