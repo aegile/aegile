d1772e4dbde1a72587507d419b1ba5bb711cba5a
Alex Xu <xulex6@gmail.com>
2024-01-03 07:02:06 +0000

    fix: remove backend password character limit due to it being stored as a password hash derivation

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -9,7 +9,7 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String, nullable=False)
     handle = db.Column(db.String(54), nullable=False, unique=True)
     image = db.Column(db.String)
 
