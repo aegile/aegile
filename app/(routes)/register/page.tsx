3b1647db61e9656d77dd8b1233de47807f46e5da
Alex Xu <xulex6@gmail.com>
2024-01-03 06:16:29 +0000

    feat: add radomised cycle of AI generated study oli painting style images

--- app/(routes)/register/page.tsx
+++ app/(routes)/register/page.tsx
@@ -4,12 +4,18 @@ import Link from 'next/link';
 
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
-import { UserAuthForm } from './components/user-registration-form';
+import { UserRegistrationForm } from './components/user-registration-form';
 
 export const metadata: Metadata = {
   title: 'Authentication',
   description: 'Authentication forms built using the components.',
 };
+
+const images = [
+  '/pages/hero-library.jpg',
+  '/pages/hero-lecture.jpg',
+  '/pages/hero-tutorial.jpg',
+];
 
 export default function AuthenticationPage() {
   return (
@@ -25,15 +31,16 @@ export default function AuthenticationPage() {
           Login
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-          {/* <div className="absolute inset-0 bg-zinc-900" /> */}
           <Image
-            src="https://unsplash.it/960/1080?random&blur&gravity=center"
+            // src="https://unsplash.it/960/1080?random&blur&gravity=center"
+            src={images[Math.floor(Math.random() * images.length)]}
             width={1920}
             height={1080}
             alt="Authentication"
-            className="absolute h-full inset-0"
+            className="absolute h-full inset-0 object-cover"
           />
-          <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-70" />
+
+          <div className="absolute inset-0 bg-gradient-to-b from-black via-transparent to-black opacity-70" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <Image
               src="/aegile-horizontal-text.svg"
@@ -62,12 +69,12 @@ export default function AuthenticationPage() {
                 Create an account
               </h1>
               <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
+                Enter your details below to create your account
               </p>
             </div>
-            <UserAuthForm />
+            <UserRegistrationForm />
             <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{' '}
+              By creating an account, you agree to our{' '}
               <Link
                 href="/terms"
                 className="underline underline-offset-4 hover:text-primary"
