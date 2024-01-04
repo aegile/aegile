c83ca3ad5067c0c4411a7f31b97cc05ca7da73f8
Alex Xu <xulex6@gmail.com>
2024-01-04 07:53:35 +0000

    feat: add basic fetch request to test deployment error

--- app/(routes)/courses/page.tsx
+++ app/(routes)/courses/page.tsx
@@ -3,14 +3,32 @@ import { getCookie } from 'cookies-next';
 import { fetchAPIRequest } from '@/lib/utils';
 
 async function getCourses() {
+  const options: RequestInit = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
   const token = getCookie('accessToken', { cookies }) || '';
-  const res = await fetchAPIRequest('/api/v1/courses', 'GET', token);
-  console.log(res);
+  if (token) {
+    options.headers = {
+      ...options.headers,
+      Authorization: `Bearer ${token}`,
+    };
+  }
+
+  const res = await fetch('http://localhost:5328/api/v1/courses', options);
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(`${res.status}: ${data.msg}`);
+  }
+  await fetchAPIRequest('/api/v1/courses', 'GET', token);
 }
 
 export default async function Home() {
   // console.log(getCookie('accessToken', { cookies }));
-  const myCourses = await getCourses();
+  // const myCourses = await getCourses();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Courses Page
