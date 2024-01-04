523fa7c0359c2bb1c091cc58e184667c5f198b1f
Alex Xu <xulex6@gmail.com>
2024-01-04 09:21:01 +0000

    fix: restore courses API fetch with access token stored in cookies

--- app/(routes)/courses/page.tsx
+++ app/(routes)/courses/page.tsx
@@ -1,9 +1,14 @@
+import { cookies } from 'next/headers';
+
 async function getCourses() {
+  const cookieStore = cookies();
+  const token = cookieStore.get('accessToken');
   const serverUrl = process.env.NEXT_PUBLIC_VERCEL_URL || '127.0.0.1:5328';
-  const res = await fetch(`http://${serverUrl}/api/v1/auth/users`, {
+  const res = await fetch(`http://${serverUrl}/api/v1/courses`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${token?.value}`,
     },
   });
   // console.log(res.json());
