559cc0c41ec4bbecc26b439d8f2d6a0d7c36ae48
Alex Xu <xulex6@gmail.com>
2024-01-04 04:34:00 +0000

    feat: add auth session cookie handling and protected routes middleware

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { getCookie } from 'cookies-next'
+import { cookies } from 'next/headers';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -19,29 +21,32 @@ export const fetchAPIRequest = async (route: string, method: string, bodyData: o
     options.body = JSON.stringify(bodyData);
   }
 
-  // const token = localStorage.getItem('token');
-  // if (token) {
-  //   options.headers = {
-  //     ...options.headers,
-  //     Authorization: `Bearer ${token}`,
-  //   };
-  // }
+  const token = getCookie('accessToken', { cookies }) || ''
+  console.log(token)
+  if (token) {
+    options.headers = {
+      ...options.headers,
+      Authorization: `Bearer ${token}`,
+    };
+  }
 
   const response = await fetch(route, options);
   const data = await response.json();
 
   if (!response.ok) {
-    throw new Error(data.message.slice(3, -4));
+    throw new Error(`${response.status}: ${response.statusText}`);
   }
 
   return data;
 };
 
 
-export async function checkAuth(token: string | null) {
+export async function checkAuth() {
+  const token = getCookie('accessToken', { cookies }) || ''
   if (!token) {
     return false;
   }
+  
   try {
     await fetchAPIRequest('/api/v1/auth/check', 'GET');
   } catch (error) {
--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,12 @@
 import { NextResponse, NextRequest } from 'next/server'
-import { parseCookies } from 'nookies'
 import { checkAuth } from '@/lib/utils';
 
 export async function middleware(req: NextRequest) {
-  const cookies = parseCookies({ req })
-  const token = cookies.token
   
-  if (!await checkAuth(token)) {
+  if (!checkAuth()) {
     return NextResponse.redirect(new URL('/login', req.url))
   }
-  req.headers.set('Authorization', `Bearer ${token}`)
-  return NextResponse.next();
+  return NextResponse.next()
 }
 
 export const config = {
