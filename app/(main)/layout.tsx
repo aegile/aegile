ecf7a5c93a5cc10601f2b08bb21eaf816a46e2b8
Alex Xu <xulex6@gmail.com>
2024-01-05 08:23:09 +0000

    feat: add menubar to authenticated pages layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,28 +1,33 @@
-import { Button } from '@/components/ui/button';
+import { cookies } from 'next/headers';
 import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+import { MenuBar } from '@/components/menu-bar';
+import { Users2 } from 'lucide-react';
 
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const layout = cookies().get('react-resizable-panels:layout');
+  const collapsed = cookies().get('react-resizable-panels:collapsed');
+
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
+
   return (
-    <section>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/main/home">Home</Link>
-          </li>
-          <li>
-            <Link href="/main/about">About</Link>
-          </li>
-          <li>
-            <Link href="/main/contact">Contact</Link>
-          </li>
-          {/* Add more links as needed */}
-        </ul>
-      </nav>
-      {children}
-    </section>
+    <div className="h-screen">
+      <MenuBar
+        accounts={[
+          { label: 'Alex Xu', email: 'alex@email.com', icon: <Users2 /> },
+        ]}
+        defaultLayout={defaultLayout}
+        defaultCollapsed={defaultCollapsed}
+        navCollapsedSize={3}
+      >
+        {children}
+      </MenuBar>
+    </div>
   );
 }
