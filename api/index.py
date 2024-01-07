5bd12d13c110443071f7608c3d9062723230a08c
Alex Xu <xulex6@gmail.com>
2024-01-07 09:53:55 +0000

    feat: temporarily replace backend server with simple flask route

--- api/index.py
+++ api/index.py
@@ -1,11 +1,20 @@
-from backend.src.extensions import db
-from backend.src import create_app
+# from backend.src.extensions import db
+# from backend.src import create_app
 
-app = create_app()
-with app.app_context():
-    db.drop_all()
-    print(">>> creating tables")
-    db.create_all()
+# app = create_app()
+# with app.app_context():
+#     db.drop_all()
+#     print(">>> creating tables")
+#     db.create_all()
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5000)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/api/python")
+def hello_world():
+    return "<p>Hello, World!</p>"
