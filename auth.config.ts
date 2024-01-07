6353db427158879438d29910b85fb1c9e9c794c6
Alex Xu <xulex6@gmail.com>
2024-01-07 08:40:42 +0000

    feat: add middleware authentication using next-auth

---
+++ auth.config.ts
@@ -0,0 +1,73 @@
+import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+
+import { LoginSchema } from '@/schemas';
+import { access } from 'fs';
+
+declare module 'next-auth' {
+  interface User {
+    /** The user's postal address. */
+    address: string;
+    access_token: string;
+    handle: string;
+  }
+  interface Session {
+    accessToken: string;
+  }
+}
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      // Add property to session, like an access_token from a provider.
+      if (user) {
+        session.user = user;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      // Add access_token to the token right after signin
+      if (user) {
+        token.accessToken = user.access_token;
+      }
+      // token.handle = (user as AegileUser)?.handle;
+      return token;
+    },
+  },
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+        if (validatedFields.success) {
+          //   const { email, password } = validatedFields.data;
+          // fetch to backend
+          const response = await fetch(
+            'http://localhost:5328/api/v1/auth/login',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(validatedFields.data),
+            }
+          );
+
+          const result = await response.json();
+          console.log(response.status, result);
+
+          if (!response.ok) {
+            // Handle error
+            console.error('Login failed');
+            return null;
+          } else {
+            return result;
+          }
+        }
+        return null;
+      },
+    }),
+  ], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
