62eac014f5dc01e43084b1ca0c64257aa05f1e48
Alex Xu <xulex6@gmail.com>
2024-01-11 03:15:40 +0000

    feat: add client side fetch request helper function for registration

--- app/(auth)/register/components/user-registration-form.tsx
+++ app/(auth)/register/components/user-registration-form.tsx
@@ -17,6 +17,7 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { toast } from 'sonner';
+import { fetchClientAPIRequest } from '@/lib/utils';
 
 const nameSchema = z
   .string()
@@ -86,24 +87,32 @@ export function UserRegistrationForm() {
       </div>
     );
     const { confirmPassword, ...bodyData } = data;
-    const response = await fetch('/api/v1/auth/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(bodyData),
-    });
+    const res = await fetchClientAPIRequest(
+      '/api/v1/auth/register',
+      'POST',
+      bodyData
+    );
+    // const response = await fetch('/api/v1/auth/register', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(bodyData),
+    // });
 
-    if (!response.ok) {
+    if (!res.ok) {
       // Handle error
       console.error('Registration failed');
-      const result = await response.json();
+      const result = await res.json();
       console.log(result);
       toast.error(result.message);
       return;
     }
 
     // handle success
+    toast.info(
+      'Account created! Please check your email for a verification link.'
+    );
     // navigate to login page
   }
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,53 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { getCookie } from 'cookies-next';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const fetchClientAPIRequest = async (
+  route: string,
+  method: string,
+  bodyData: object = {}
+) => {
+  const options: RequestInit = {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  if (method === 'GET') {
+    // append payload to path
+  } else {
+    options.body = JSON.stringify(bodyData);
+  }
+
+  // const jwtCookie = getCookie('_vercel_jwt');
+  const authToken = getCookie('accessToken');
+
+  options.headers = {
+    ...options.headers,
+    ...(authToken ? { Authorization: `Bearer ${authToken}` } : {}),
+    // ...(jwtCookie ? { Cookie: `_vercel_jwt=${jwtCookie}` } : {}),
+  };
+
+  const routeProtocolPrefix =
+    process.env.NODE_ENV === 'production' && process.env.LOCAL !== 'true'
+      ? 'https://'
+      : 'http://';
+
+  const url = `${routeProtocolPrefix}${process.env.NEXT_PUBLIC_VERCEL_URL}${route}`;
+  console.log(url);
+  const response = await fetch(url, options);
+
+  return response;
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(`${response.status}: ${data.msg}`);
+  }
+
+  return data;
+};
