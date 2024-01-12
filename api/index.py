9408d12fd6727c92674357d3f760521ea7e86f37
Alex Xu <xulex6@gmail.com>
2024-01-12 08:48:02 +0000

    feat: add test user on backend startup

--- api/index.py
+++ api/index.py
@@ -1,11 +1,19 @@
+from werkzeug.security import generate_password_hash
 from backend.src.extensions import db
 from backend.src import create_app
+from backend.src.models.user import User
 
 app = create_app()
 with app.app_context():
     db.drop_all()
     print(">>> creating tables")
     db.create_all()
+    user = User(
+        "Lex", "Xu", "z5555555@ad.unsw.edu.au", generate_password_hash("AlexXu123!")
+    )
+    db.session.add(user)
+    db.session.commit()
+    print(db.session.execute(db.select(User)).all())
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
