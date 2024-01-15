b1af8c713c8717748693b3dca6ada9cecc53ccc8
Alex Xu <xulex6@gmail.com>
2024-01-15 11:11:16 +0000

    feat: add better scaling for data not found component

--- components/page-ui/data-not-found.tsx
+++ components/page-ui/data-not-found.tsx
@@ -1,21 +1,21 @@
 import Image from 'next/image';
 import React from 'react';
 
-function DataNotFound({ dataName }: { dataName: string }) {
+export default function DataNotFound({ dataName }: { dataName: string }) {
   return (
-    <div className="flex flex-col justify-center items-center w-full h-full">
+    <div className="flex flex-col justify-center items-center h-full w-full">
       <Image
         src="/illustrations/not-found/not-found-blank-canvas.svg"
         // src={`/illustrations/not-found/not-found-${dataName}.svg`} // not-found-courses.svg, not-found-deliverables.svg, not-found-submissions.svg
         alt="Data not found."
-        width={300}
-        height={300}
+        width={200}
+        height={200}
+        className="object-scale-down"
       />
       <h2 className="mt-10 mx-auto text-3xl font-semibold tracking-tight">
         No {dataName} found ...
       </h2>
       <p className="leading-7 [&:not(:first-child)]:mt-4 text-balance">
-        {/* TODO - if the user has the permissions, they can create a course */}
         Contact your relevant administrators to add you to your {dataName}.
       </p>
       <p className="leading-7 [&:not(:first-child)]:mt-4 text-balance">
@@ -24,5 +24,3 @@ function DataNotFound({ dataName }: { dataName: string }) {
     </div>
   );
 }
-
-export default DataNotFound;
