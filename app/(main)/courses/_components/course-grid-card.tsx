ab49471de32fbcac741749a33a2a2657d6ee4e8c
Alex Xu <xulex6@gmail.com>
2024-01-15 11:09:04 +0000

    feat: reduce height of course grid card image

--- app/(main)/courses/_components/course-grid-card.tsx
+++ app/(main)/courses/_components/course-grid-card.tsx
@@ -23,20 +23,20 @@ function CourseGridCard({ id, term, code, name, member_count }: Course) {
     <Link href={`/courses/${id}`}>
       <Card className="h-full flex flex-col hover:bg-accent hover:text-accent-foreground">
         <Image
-          src="https://unsplash.it/250/125"
+          src="https://unsplash.it/250/100"
           width={250}
-          height={125}
+          height={100}
           alt="Test"
-          className="rounded-t-xl w-full h-auto object-cover"
+          className="rounded-t-xl w-full object-cover"
         />
-        <CardHeader className="pb-3">
+        <CardHeader className="pb-0">
           <CardTitle>
             {term}-{code}
           </CardTitle>
           <CardDescription>{name}</CardDescription>
         </CardHeader>
         <TooltipProvider>
-          <CardContent className="mt-auto flex space-x-3">
+          <CardContent className="mt-auto">
             <IconTextTooltip
               isFlex
               tooltips={[
