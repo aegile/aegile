6a10778b5b6e128435c019c10ad86d8de03d499d
Alex Xu <xulex6@gmail.com>
2024-01-17 12:50:10 +0000

    feat: improve courses page grid responsiveness

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -20,6 +20,85 @@ async function getCourses() {
 
   const data = await res.json();
   return data;
+  return [
+    {
+      id: 'string',
+      term: 'string',
+      code: 'string',
+      name: 'string',
+      member_count: 1,
+    },
+    {
+      id: 'string',
+      term: 'string',
+      code: 'string',
+      name: 'string',
+      member_count: 1,
+    },
+    {
+      id: 'string',
+      term: 'string',
+      code: 'string',
+      name: 'string',
+      member_count: 1,
+    },
+    {
+      id: 'string',
+      term: 'string',
+      code: 'string',
+      name: 'string',
+      member_count: 1,
+    },
+    {
+      id: 'string',
+      term: 'string',
+      code: 'string',
+      name: 'string',
+      member_count: 1,
+    },
+    {
+      id: 'string',
+      term: 'string',
+      code: 'string',
+      name: 'string',
+      member_count: 1,
+    },
+    {
+      id: 'string',
+      term: 'string',
+      code: 'string',
+      name: 'string',
+      member_count: 1,
+    },
+    {
+      id: 'string',
+      term: 'string',
+      code: 'string',
+      name: 'string',
+      member_count: 1,
+    },
+    {
+      id: 'string',
+      term: 'string',
+      code: 'string',
+      name: 'string',
+      member_count: 1,
+    },
+    {
+      id: 'string',
+      term: 'string',
+      code: 'string',
+      name: 'string',
+      member_count: 1,
+    },
+    {
+      id: 'string',
+      term: 'string',
+      code: 'string',
+      name: 'string',
+      member_count: 1,
+    },
+  ];
 }
 
 export const metadata: Metadata = {
@@ -31,16 +110,16 @@ export default async function CoursesPage() {
 
   return (
     <div className="h-screen flex flex-col p-8 space-y-6 overflow-y-auto">
-      {/* <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between">
         <h2 className="text-3xl font-semibold tracking-tight">My Courses</h2>
-      </div> */}
-      <div className="flex flex-wrap sm:grid sm:grid-cols-2 lg:grid-cols-3 grid-rows-4 sm:grid-rows-2 h-fit sm:h-full gap-5">
-        <div className="w-full col-span-full lg:col-span-2 row-span-2 lg:row-span-full max-h-[500px] sm:max-h-full">
+      </div>
+      <div className="flex-grow space-y-5 sm:space-y-0 sm:grid sm:grid-cols-2 lg:grid-cols-3 sm:grid-rows-2 gap-5 sm:overflow-y-auto">
+        <div className="sm:col-span-2 lg:row-span-2">
           <CoursesMain courses={courses} />
         </div>
-
         <CoursesCalendar courses={courses} />
         <CoursesRecents courses={courses} />
+        {/* <div className="w-full h-full border border-lime-600">Timeline</div> */}
         {/* <div className="w-full h-full border border-lime-600">Recents</div> */}
       </div>
     </div>
