471de77308a0f1b0ab54d3d3669a07d2e915afd0
Alex Xu <xulex6@gmail.com>
2024-01-17 12:49:04 +0000

    feat: move row actions to far right side of row

--- app/(main)/courses/[course_id]/participants/course_participants-row-actions.tsx
+++ app/(main)/courses/[course_id]/participants/course_participants-row-actions.tsx
@@ -35,7 +35,7 @@ export function DataTableRowActions<TData>({
       <DropdownMenuTrigger asChild>
         <Button
           variant="ghost"
-          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted ml-auto"
         >
           <DotsHorizontalIcon className="h-4 w-4" />
           <span className="sr-only">Open menu</span>
