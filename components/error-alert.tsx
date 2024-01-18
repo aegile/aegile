9e274b586418995f122e1d16ebb19133212959d2
Alex Xu <xulex6@gmail.com>
2024-01-18 11:50:10 +0000

    feat: add trial error client component to render server side errors during fetch

---
+++ components/error-alert.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import { logout } from '@/lib/actions/logout';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { toast } from 'sonner';
+
+export default function ErrorAlert({ error }: { error: { error: string } }) {
+  useEffect(() => {
+    toast.error(error.error);
+  }, []);
+
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <Card className="w-[500px]">
+        <CardHeader>
+          <CardTitle className="text-2xl font-semibold">
+            {error.error || 'Unexpected Error'}
+          </CardTitle>
+          <CardDescription>An unexpected error has occurred.</CardDescription>
+        </CardHeader>
+        <CardContent>Please try logging in again.</CardContent>
+        <CardFooter className="flex justify-around gap-3">
+          <Button className="w-full" onClick={() => logout()}>
+            Log In
+          </Button>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+}
