2f835756fb84dfcff9f1b1be813bd274ba459900
Alex Xu <xulex6@gmail.com>
2024-01-18 11:46:13 +0000

    feat: update courses and classes pages with generic grid and list view card

--- app/(main)/courses/[course_id]/classes/page.tsx
+++ app/(main)/courses/[course_id]/classes/page.tsx
@@ -1,3 +1,45 @@
+import { fetchServerAPIRequest } from '@/lib/server-utils';
+
+import { Button } from '@/components/ui/button';
+import { Presentation } from 'lucide-react';
+import UpcomingClasses from './upcoming-classes';
+import { ClassCreationDialog } from './class-creation-dialog';
+import GridListViewCard from '@/components/grid-list-view-card';
+
+// async function getTutorials() {
+//   const res = await fetchServerAPIRequest('/api/v1/tutorials', 'GET');
+//   if (res.status === 401)
+//     throw new Error("You don't have permission to view this page.");
+//   if (res.status === 400)
+//     return { error: "You don't have permission to view this page." };
+//   const data = await res.json();
+//   return data;
+// }
+
 export default async function CourseClassesPage() {
-  return <div className="h-full flex justify-center items-center">Classes</div>;
+  // const tutorials = await getTutorials();
+
+  return (
+    <div className="h-full flex flex-col space-y-3">
+      <div className="flex justify-between">
+        <h3 className="text-2xl font-semibold tracking-tight flex items-center">
+          <Presentation className="mr-2" /> Classes
+        </h3>
+      </div>
+      <div className="flex-grow space-y-5 md:space-y-0 md:grid md:grid-cols-2 lg:grid-cols-3 md:grid-rows-2 gap-5 md:overflow-y-auto">
+        <div className="md:col-span-2 row-span-full">
+          <GridListViewCard
+            dataName="classes"
+            items={[]}
+            GridItemComponent={() => <></>}
+            ListItemComponent={() => <></>}
+            ItemCreationComponent={ClassCreationDialog}
+          />
+        </div>
+        {/* <div className="w-full row-span-full"></div> */}
+        <UpcomingClasses />
+        <UpcomingClasses />
+      </div>
+    </div>
+  );
 }
--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -2,21 +2,23 @@ import { Metadata } from 'next';
 import { fetchServerAPIRequest } from '@/lib/server-utils';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
-import { LayoutGrid, LayoutList } from 'lucide-react';
+import { Grid, LayoutGrid, LayoutList } from 'lucide-react';
 
 import CourseGridCard from './_components/course-grid-card';
 import CourseListCard from './_components/course-list-card';
 import DataNotFound from '@/components/page-ui/data-not-found';
 
 import { CourseCreationDialog } from './_components/course-creation-dialog';
-import CoursesMain from './_components/courses-main';
 import CoursesCalendar from './_components/courses-calendar';
 import CoursesRecents from './_components/courses-recents';
+import ErrorAlert from '@/components/error-alert';
+import GridListViewCard from '@/components/grid-list-view-card';
 
 async function getCourses() {
   const res = await fetchServerAPIRequest('/api/v1/courses', 'GET');
   if (res.status === 401)
-    throw new Error("You don't have permission to view this page.");
+    return { error: "You don't have permission to view this page." };
+  // throw new Error("You don't have permission to view this page.");
 
   const data = await res.json();
   return data;
@@ -106,8 +108,8 @@ export const metadata: Metadata = {
 };
 
 export default async function CoursesPage() {
-  const courses: Course[] = await getCourses();
-
+  const courses = await getCourses();
+  if (courses.error) return <ErrorAlert error={courses} />;
   return (
     <div className="h-screen flex flex-col p-8 space-y-6 overflow-y-auto">
       <div className="flex items-center justify-between">
@@ -115,12 +117,16 @@ export default async function CoursesPage() {
       </div>
       <div className="flex-grow space-y-5 sm:space-y-0 sm:grid sm:grid-cols-2 lg:grid-cols-3 sm:grid-rows-2 gap-5 sm:overflow-y-auto">
         <div className="sm:col-span-2 lg:row-span-2">
-          <CoursesMain courses={courses} />
+          <GridListViewCard<Course>
+            dataName="courses"
+            items={courses}
+            GridItemComponent={CourseGridCard}
+            ListItemComponent={CourseListCard}
+            ItemCreationComponent={CourseCreationDialog}
+          />
         </div>
         <CoursesCalendar courses={courses} />
         <CoursesRecents courses={courses} />
-        {/* <div className="w-full h-full border border-lime-600">Timeline</div> */}
-        {/* <div className="w-full h-full border border-lime-600">Recents</div> */}
       </div>
     </div>
   );
