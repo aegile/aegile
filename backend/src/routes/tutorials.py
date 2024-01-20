c678e188c24e6db29e4df7ac85895bc19a4c5f50
Alex Xu <xulex6@gmail.com>
2024-01-20 08:55:07 +0000

    feat: add tutorial deletion rotue

--- backend/src/routes/tutorials.py
+++ backend/src/routes/tutorials.py
@@ -87,4 +87,8 @@ class TutorialSpecific(Resource):
         return update_db_object(Tutorial, tutorial.__repr__())
 
     def delete(self, tutorial_id: str):
-        pass
+        tutorial: Tutorial = fetch_one(Tutorial, {"id": tutorial_id})
+        check_authorization(tutorial.course, current_user, "can_manage_tutorials")
+        db.session.delete(tutorial)
+        db.session.commit()
+        return {}, 200
