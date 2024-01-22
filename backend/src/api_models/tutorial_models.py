90fec91d308bd16df05f796fbad40a7d77f0424c
Philip Tran <philiptran218@gmail.com>
2024-01-22 12:47:24 +0000

    feat: add routes for tutorials

--- backend/src/api_models/tutorial_models.py
+++ backend/src/api_models/tutorial_models.py
@@ -1,11 +1,12 @@
 from flask_restx import fields
 from ..extensions import api
-from .user_models import userset_list_output
+from .user_models import user_fetch_output
 
 tutorial_fetch_output = api.model(
     "TutorialFetchOutput",
     {
         "id": fields.Integer,
+        "course_id": fields.Integer,
         "name": fields.String,
         "capacity": fields.Integer,
         "datetime": fields.String,
@@ -20,6 +21,32 @@ tutorial_creation_input = api.model(
         "capacity": fields.Integer(required=True),
         "datetime": fields.String(required=True),
         "location": fields.String(required=True),
+    },
+    strict=True,
+)
+
+tutorial_edit_input = api.model(
+    "TutorialEditInput",
+    {
+        "name": fields.String,
+        "capacity": fields.Integer,
+        "datetime": fields.String,
+        "location": fields.String,
+    },
+    strict=True,
+)
+
+tutorial_members_fetch_output = api.model(
+    "TutorialMembersFetchOutput",
+    {
+        "members": fields.List(fields.Nested(user_fetch_output)),
+    },
+)
+
+tutorial_members_input = api.model(
+    "TutorialMembersInput",
+    {
+        "members": fields.List(fields.String(required=True)),
     },
     strict=True,
 )
