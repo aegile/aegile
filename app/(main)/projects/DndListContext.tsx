0f5d4a19652b827195ea6afd3f354b01db118e3e
Alex Xu <xulex6@gmail.com>
2024-02-24 12:30:27 +0000

    feat: add task list and task card drag and drop functionality

---
+++ app/(main)/projects/DndListContext.tsx
@@ -0,0 +1,462 @@
+'use client';
+import { useState } from 'react';
+// import { v4 as uuidv4 } from 'uuid';
+
+import {
+  DndContext,
+  DragEndEvent,
+  DragMoveEvent,
+  DragOverlay,
+  DragStartEvent,
+  KeyboardSensor,
+  PointerSensor,
+  UniqueIdentifier,
+  closestCorners,
+  pointerWithin,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  SortableContext,
+  arrayMove,
+  horizontalListSortingStrategy,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+
+import Container from './components/container';
+import Items from './components/item';
+import { PlusIcon } from '@radix-ui/react-icons';
+
+type DNDType = {
+  id: UniqueIdentifier;
+  title: string;
+  items: {
+    id: UniqueIdentifier;
+    title: string;
+  }[];
+};
+
+export default function DndListContext() {
+  const [containers, setContainers] = useState<DNDType[]>([
+    {
+      id: 'container-0',
+      title: 'hello0',
+      items: [
+        { id: 'item-0', title: 'there0' },
+        { id: 'item-1', title: 'there1' },
+        { id: 'item-2', title: 'there2' },
+        { id: 'item-3', title: 'there3' },
+        { id: 'item-4', title: 'there4' },
+      ],
+    },
+    { id: 'container-1', title: 'hello1', items: [] },
+    {
+      id: 'container-2-asglkasghljaksgasgl aslgihashglhasgh  ahsioghoias hoighaios',
+      title: 'hello2',
+      items: [],
+    },
+    { id: 'container-3', title: 'hello3', items: [] },
+    { id: 'container-4', title: 'hello4', items: [] },
+    { id: 'container-5', title: 'hello5', items: [] },
+    { id: 'container-6', title: 'hello6', items: [] },
+    { id: 'container-7', title: 'hello7', items: [] },
+    { id: 'container-8', title: 'hello8', items: [] },
+    { id: 'container-9', title: 'hello9', items: [] },
+  ]);
+  const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
+  const [currentContainerId, setCurrentContainerId] =
+    useState<UniqueIdentifier>();
+  const [containerName, setContainerName] = useState('');
+  const [itemName, setItemName] = useState('');
+  const [showAddItemModal, setShowAddItemModal] = useState(false);
+
+  const onAddContainer = () => {
+    if (!containerName) return;
+    const id = `container-${Math.random().toString(36).slice(2)}`;
+    setContainers([
+      ...containers,
+      {
+        id,
+        title: containerName,
+        items: [],
+      },
+    ]);
+    setContainerName('');
+  };
+  const onAddItem = () => {
+    if (!itemName) return;
+    const id = `item-${Math.random().toString(36).slice(2)}`;
+    const container = containers.find((item) => item.id === currentContainerId);
+    if (!container) return;
+    container.items.push({
+      id,
+      title: itemName,
+    });
+    setContainers([...containers]);
+    setItemName('');
+    setShowAddItemModal(false);
+  };
+
+  function findValueOfItems(id: UniqueIdentifier | undefined, type: string) {
+    if (type === 'container') {
+      return containers.find((item) => item.id === id);
+    }
+    if (type === 'item') {
+      return containers.find((container) =>
+        container.items.find((item) => item.id === id)
+      );
+    }
+  }
+
+  const findItemTitle = (id: UniqueIdentifier | undefined) => {
+    const container = findValueOfItems(id, 'item');
+    if (!container) return '';
+    const item = container.items.find((item) => item.id === id);
+    if (!item) return '';
+    return item.title;
+  };
+
+  const findContainerTitle = (id: UniqueIdentifier | undefined) => {
+    const container = findValueOfItems(id, 'container');
+    if (!container) return '';
+    return container.title;
+  };
+
+  const findContainerItems = (id: UniqueIdentifier | undefined) => {
+    const container = findValueOfItems(id, 'container');
+    if (!container) return [];
+    return container.items;
+  };
+
+  // DND Related handlers
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, { coordinateGetter: sortableKeyboardCoordinates })
+  );
+  const handleDragStart = (event: DragStartEvent) => {
+    const { active } = event;
+    const { id } = active;
+    setActiveId(id);
+  };
+
+  const handleDragMove = (event: DragMoveEvent) => {
+    const { active, over } = event;
+
+    if (
+      active.id.toString().includes('item') &&
+      over?.id.toString().includes('item') &&
+      active &&
+      over &&
+      active.id !== over.id
+    ) {
+      const activeContainer = findValueOfItems(active.id, 'item');
+      const overContainer = findValueOfItems(over.id, 'item');
+
+      if (!activeContainer || !overContainer) return;
+
+      const activeContainerIndex = containers.findIndex(
+        (container) => container.id === activeContainer.id
+      );
+      const overContainerIndex = containers.findIndex(
+        (container) => container.id === overContainer.id
+      );
+
+      const activeitemIndex = activeContainer.items.findIndex(
+        (item) => item.id === active.id
+      );
+      const overitemIndex = overContainer.items.findIndex(
+        (item) => item.id === over.id
+      );
+      // same container
+      if (activeContainerIndex === overContainerIndex) {
+        let newItems = [...containers];
+        newItems[activeContainerIndex].items = arrayMove(
+          newItems[activeContainerIndex].items,
+          activeitemIndex,
+          overitemIndex
+        );
+
+        setContainers(newItems);
+      } else {
+        // diff containers
+        let newItems = [...containers];
+        const [removeditem] = newItems[activeContainerIndex].items.splice(
+          activeitemIndex,
+          1
+        );
+        newItems[overContainerIndex].items.splice(
+          overitemIndex,
+          0,
+          removeditem
+        );
+        setContainers(newItems);
+      }
+    }
+
+    // drag into droppable
+    if (
+      active.id.toString().includes('item') &&
+      over?.id.toString().includes('container') &&
+      active &&
+      over &&
+      active.id !== over.id
+    ) {
+      const activeContainer = findValueOfItems(active.id, 'item');
+      const overContainer = findValueOfItems(over.id, 'container');
+
+      if (!activeContainer || !overContainer) return;
+
+      const activeContainerIndex = containers.findIndex(
+        (container) => container.id === activeContainer.id
+      );
+      const overContainerIndex = containers.findIndex(
+        (container) => container.id === overContainer.id
+      );
+
+      const activeitemIndex = activeContainer.items.findIndex(
+        (item) => item.id === active.id
+      );
+
+      let newItems = [...containers];
+      const [removeditem] = newItems[activeContainerIndex].items.splice(
+        activeitemIndex,
+        1
+      );
+      newItems[overContainerIndex].items.push(removeditem);
+      setContainers(newItems);
+    }
+  };
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    if (
+      active.id.toString().includes('container') &&
+      over?.id.toString().includes('container') &&
+      active &&
+      over &&
+      active.id !== over.id
+    ) {
+      const activeContainerIndex = containers.findIndex(
+        (container) => container.id === active.id
+      );
+      const overContainerIndex = containers.findIndex(
+        (container) => container.id === over.id
+      );
+      let newItems = [...containers];
+      newItems = arrayMove(newItems, activeContainerIndex, overContainerIndex);
+      setContainers(newItems);
+    }
+
+    if (
+      active.id.toString().includes('item') &&
+      over?.id.toString().includes('item') &&
+      active &&
+      over &&
+      active.id !== over.id
+    ) {
+      const activeContainer = findValueOfItems(active.id, 'item');
+      const overContainer = findValueOfItems(over.id, 'item');
+
+      if (!activeContainer || !overContainer) return;
+      const activeContainerIndex = containers.findIndex(
+        (container) => container.id === activeContainer.id
+      );
+      const overContainerIndex = containers.findIndex(
+        (container) => container.id === overContainer.id
+      );
+      const activeitemIndex = activeContainer.items.findIndex(
+        (item) => item.id === active.id
+      );
+      const overitemIndex = overContainer.items.findIndex(
+        (item) => item.id === over.id
+      );
+
+      // in same container
+      if (activeContainerIndex === overContainerIndex) {
+        let newItems = [...containers];
+        newItems[activeContainerIndex].items = arrayMove(
+          newItems[activeContainerIndex].items,
+          activeitemIndex,
+          overitemIndex
+        );
+        setContainers(newItems);
+      } else {
+        // in diff containers
+        let newItems = [...containers];
+        const [removeditem] = newItems[activeContainerIndex].items.splice(
+          activeitemIndex,
+          1
+        );
+        newItems[overContainerIndex].items.splice(
+          overitemIndex,
+          0,
+          removeditem
+        );
+        setContainers(newItems);
+      }
+    }
+    // drop item in container
+    if (
+      active.id.toString().includes('item') &&
+      over?.id.toString().includes('container') &&
+      active &&
+      over &&
+      active.id !== over.id
+    ) {
+      const activeContainer = findValueOfItems(active.id, 'item');
+      const overContainer = findValueOfItems(over.id, 'container');
+
+      if (!activeContainer || !overContainer) return;
+      const activeContainerIndex = containers.findIndex(
+        (container) => container.id === activeContainer.id
+      );
+      const overContainerIndex = containers.findIndex(
+        (container) => container.id === overContainer.id
+      );
+      const activeitemIndex = activeContainer.items.findIndex(
+        (item) => item.id === active.id
+      );
+
+      let newItems = [...containers];
+      const [removeditem] = newItems[activeContainerIndex].items.splice(
+        activeitemIndex,
+        1
+      );
+      newItems[overContainerIndex].items.push(removeditem);
+      setContainers(newItems);
+    }
+    setActiveId(null);
+  }
+
+  return (
+    <div className="w-full h-full">
+      <Dialog>
+        <DialogTrigger>
+          <Button variant="outline">
+            <PlusIcon className="mr-2 w-5 h-5" /> New List
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>Create a task list</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+          <Input
+            id="list-name"
+            type="text"
+            placeholder="New Task List Title"
+            name="containername"
+            value={containerName}
+            onChange={(e) => setContainerName(e.target.value)}
+          />
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button type="submit" onClick={onAddContainer}>
+                <span className="sr-only">Create new task list</span>
+                Create
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={showAddItemModal} onOpenChange={setShowAddItemModal}>
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>Create a task list</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+          <Input
+            id="item-name"
+            type="text"
+            placeholder="New Item Name"
+            name="itemname"
+            value={itemName}
+            onChange={(e) => setItemName(e.target.value)}
+          />
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button type="submit" onClick={onAddItem}>
+                <span className="sr-only">Create new task list</span>
+                Create
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <div className="h-full p-2 flex overflow-x-auto gap-8 touch-pan-x scroll-auto">
+        <DndContext
+          sensors={sensors}
+          collisionDetection={pointerWithin}
+          onDragStart={handleDragStart}
+          onDragMove={handleDragMove}
+          onDragEnd={handleDragEnd}
+        >
+          <SortableContext
+            items={containers.map((container) => container.id)}
+            // strategy={horizontalListSortingStrategy}
+          >
+            {containers.map((container) => (
+              <Container
+                key={container.id}
+                id={container.id}
+                title={container.title}
+                onAddItem={() => {
+                  setShowAddItemModal(true);
+                  setCurrentContainerId(container.id);
+                }}
+              >
+                <SortableContext
+                  items={container.items.map((i) => i.id)}
+                  // strategy={verticalListSortingStrategy}
+                >
+                  <div className="flex items-start flex-col gap-y-4 h-full">
+                    {container.items.map((i) => (
+                      <Items title={i.title} id={i.id} key={i.id} />
+                    ))}
+                  </div>
+                </SortableContext>
+              </Container>
+            ))}
+          </SortableContext>
+          <DragOverlay adjustScale={false}>
+            {activeId && activeId.toString().includes('item') && (
+              <Items id={activeId} title={findItemTitle(activeId)} />
+            )}
+            {activeId && activeId.toString().includes('container') && (
+              <Container
+                id={activeId}
+                title={findContainerTitle(activeId)}
+                isOverlay
+              />
+            )}
+          </DragOverlay>
+        </DndContext>
+      </div>
+    </div>
+  );
+}
+
+// {findContainerItems(activeId).map((i) => (
+//   <Items key={i.id} title={i.title} id={i.id} />
+// ))}
---
+++ app/(main)/projects/components/container.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import clsx from 'clsx';
+import { Button } from '@/components/ui/button';
+import { UniqueIdentifier } from '@dnd-kit/core';
+import { DotsHorizontalIcon, DragHandleDots2Icon } from '@radix-ui/react-icons';
+import { PlusIcon } from 'lucide-react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+
+interface ContainerProps {
+  id: UniqueIdentifier;
+  children?: React.ReactNode;
+  title?: string;
+  description?: string;
+  onAddItem?: () => void;
+  isOverlay?: boolean;
+}
+
+const Container = ({
+  id,
+  children,
+  title,
+  description,
+  onAddItem,
+  isOverlay = false,
+}: ContainerProps) => {
+  const {
+    attributes,
+    setNodeRef,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: id,
+    data: {
+      type: 'container',
+    },
+  });
+  return (
+    <div
+      {...attributes}
+      ref={setNodeRef}
+      style={{
+        transition,
+        transform: CSS.Translate.toString(transform),
+      }}
+      className={clsx(
+        !isOverlay && 'bg-gray-50 hover:shadow-md hover:shadow-slate-400',
+        'min-w-[250px] max-w-[250px] sm:min-w-[350px] sm:max-w-[350px] p-4 h-full rounded-xl flex flex-col gap-y-4',
+        isDragging && 'opacity-50 border border-primary'
+      )}
+    >
+      <TooltipProvider>
+        <div
+          className={clsx(
+            'flex justify-between items-center mb-3 bg-gray-50 rounded-xl',
+            isOverlay && 'p-4 shadow-lg shadow-slate-300'
+          )}
+        >
+          <div className="flex items-center gap-y-1">
+            <DragHandleDots2Icon
+              className="w-5 h-5 text-muted cursor-grab"
+              {...listeners}
+            />
+            <h1 className="text-gray-500 font-semibold truncate">{title}</h1>
+          </div>
+          <div className="flex gap-x-2">
+            <Tooltip>
+              <TooltipTrigger>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="h-6 w-6"
+                  onClick={onAddItem}
+                >
+                  <PlusIcon className="w-5 h-5 text-muted" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>New Item</p>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger>
+                <Button variant="ghost" size="icon" className="h-6 w-6">
+                  <DotsHorizontalIcon className="w-5 h-5 text-muted" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>List options</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+        </div>
+
+        {children}
+      </TooltipProvider>
+    </div>
+  );
+};
+
+export default Container;
