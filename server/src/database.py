55c4e89349124a993359171a0f710b563f0fd52f
Alex <47023455+LexicusRex@users.noreply.github.com>
2024-04-07 10:12:40 +0000

    feat: add async postgres plugin and postgres db connection

--- server/src/database.py
+++ server/src/database.py
@@ -1,3 +1,4 @@
+import os
 from pydantic import BaseModel
 
 # from sqlalchemy import create_engine, Column, Integer, String, select
@@ -6,18 +7,27 @@ from sqlalchemy.engine import Engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
-# SQLALCHEMY
+# """Toggle comment block to test Postgres connection"""
+# from dotenv import load_dotenv
+# load_dotenv(".env.local")
+
+"""Toggle comment block to switch between local and production databases"""
 engine = create_async_engine(
-    "sqlite+aiosqlite:///db.sqlite3", connect_args={"check_same_thread": False}
+    os.environ.get("POSTGRES_URL").replace("postgres://", "postgresql+asyncpg://", 1),
+    # connect_args={"check_same_thread": False},
 )
+
+# """Toggle comment block to test local SQLite connection"""
+# engine = create_async_engine(
+#     "sqlite+aiosqlite:///db.sqlite3", connect_args={"check_same_thread": False}
+# )
+# @event.listens_for(Engine, "connect")
+# def set_sqlite_pragma(dbapi_connection, connection_record):
+#     cursor = dbapi_connection.cursor()
+#     cursor.execute("PRAGMA foreign_keys=ON")
+#     cursor.close()
+
 SessionLocal = async_sessionmaker(engine)
-
-
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
 
 
 async def get_db_session():
