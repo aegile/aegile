282d56f9127f292cb03835b81eae69d7409679c6
Alex <47023455+LexicusRex@users.noreply.github.com>
2024-04-07 12:05:44 +0000

    test

--- api/index.py
+++ api/index.py
@@ -101,9 +101,9 @@
 
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
 
 @app.get("/api/python")
 def hello_world():
-    return {"message": "Hello World"}
+    return {"status": "success", "message": "Hello World"}
