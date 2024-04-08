41e93e275f0a0f49214a96329d57c1cdb89dac97
Alex <47023455+LexicusRex@users.noreply.github.com>
2024-04-08 03:22:02 +0000

    add

--- next.config.js
+++ next.config.js
@@ -38,25 +38,25 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: '/api/py/:path*',
+        source: '/api/:path*',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/api/py/:path*'
+            ? 'http://127.0.0.1:8000/api/:path*'
             : '/api/',
       },
       {
         source: '/docs',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/api/py/docs'
-            : '/api/py/docs',
+            ? 'http://127.0.0.1:8000/api/docs'
+            : '/api/docs',
       },
       {
         source: '/openapi.json',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/api/py/openapi.json'
-            : '/api/py/openapi.json',
+            ? 'http://127.0.0.1:8000/api/openapi.json'
+            : '/api/openapi.json',
       },
     ];
   },
