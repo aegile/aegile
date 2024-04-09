8bcef7c132d0f497485c97240b6ea2ff821341c1
Alex <47023455+LexicusRex@users.noreply.github.com>
2024-04-09 06:44:06 +0000

    feat: add page header components to auth pages layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,9 +1,33 @@
-import { cookies } from 'next/headers';
+import * as React from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
+import { Search } from 'lucide-react';
 
+import { Badge } from '@/components/ui/badge';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
 import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Input } from '@/components/ui/input';
+
+import { cookies } from 'next/headers';
 import { MenuBar } from '@/components/menu-bar';
-import { Users2 } from 'lucide-react';
+import NavBar from '@/components/navbar';
+import NavBarMobile from '@/components/navbar-mobile';
 
 export default function MainLayout({
   children,
@@ -15,19 +39,57 @@ export default function MainLayout({
 
   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
   const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
-
   return (
-    <div className="h-screen">
-      <MenuBar
-        accounts={[
-          { label: 'Alex Xu', email: 'alex@email.com', icon: <Users2 /> },
-        ]}
-        defaultLayout={defaultLayout}
-        defaultCollapsed={defaultCollapsed}
-        navCollapsedSize={3}
-      >
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <NavBar />
+      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+          <NavBarMobile />
+          <Breadcrumb className="hidden md:flex">
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href="#">Dashboard</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+          <div className="relative ml-auto flex-1 md:grow-0">
+            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              type="search"
+              placeholder="Search..."
+              className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
+            />
+          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="overflow-hidden rounded-full"
+              >
+                <Image
+                  src="/aegile-logo-only.svg"
+                  width={25}
+                  height={25}
+                  alt="Avatar"
+                  className="overflow-hidden rounded-full"
+                />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </header>
         {children}
-      </MenuBar>
+      </div>
     </div>
   );
 }
