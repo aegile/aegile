90dc41f60472f9f64c8d8afcb656159996eb3c33
Alex <47023455+LexicusRex@users.noreply.github.com>
2024-04-10 03:29:24 +0000

    feat: add placeholder layout for course edit page

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -34,11 +34,11 @@ export default function MainLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const layout = cookies().get('react-resizable-panels:layout');
-  const collapsed = cookies().get('react-resizable-panels:collapsed');
+  // const layout = cookies().get('react-resizable-panels:layout');
+  // const collapsed = cookies().get('react-resizable-panels:collapsed');
 
-  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
-  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
+  // const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  // const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <NavBar />
