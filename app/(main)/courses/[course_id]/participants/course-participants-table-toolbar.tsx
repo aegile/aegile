cae6cb4fc834ece53216089323457fa02afd2f5a
Alex <47023455+LexicusRex@users.noreply.github.com>
2024-04-14 12:03:00 +0000

    feat: set background to app global background variable

--- app/(main)/courses/[course_id]/participants/course-participants-table-toolbar.tsx
+++ app/(main)/courses/[course_id]/participants/course-participants-table-toolbar.tsx
@@ -38,7 +38,7 @@ export function DataTableToolbar<TData>({
           onChange={(event) =>
             table.getColumn('email')?.setFilterValue(event.target.value)
           }
-          className="h-8 w-[150px] lg:w-[250px]"
+          className="h-8 w-[150px] lg:w-[250px] bg-background"
         />
         {table.getColumn('first_name') && (
           <DataTableFacetedFilter
