bacc88bfc3b6fcfe455fdada2d2eaef81df5de0c
Alex <47023455+LexicusRex@users.noreply.github.com>
2024-04-14 11:31:04 +0000

    chore: remove unused imports

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,15 +3,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Search } from 'lucide-react';
 
-import { Badge } from '@/components/ui/badge';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -43,7 +34,7 @@ export default function MainLayout({
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <NavBar />
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+      <div className="flex flex-col sm:gap-4 sm:pt-4 sm:pl-14">
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
           <NavBarMobile />
           <BreadcrumbPath />
