2e43e7ecf58d09798404d3966590f9e03d40c5ba
Alex <47023455+LexicusRex@users.noreply.github.com>
2024-05-04 13:21:35 +0000

    feat: add stub participant data to participants table

--- app/(main)/courses/[course_id]/participants/page.tsx
+++ app/(main)/courses/[course_id]/participants/page.tsx
@@ -24,7 +24,62 @@ export default async function CourseParticipantsPage({
   params: { course_id: string };
 }) {
   // const members: Participant[] = await getParticipants(params.course_id);
-  const members: Participant[] = [];
+  const members: Participant[] = [
+    {
+      id: 'usr_as9gyagya089ygss',
+      email: 'alex@email.com',
+      first_name: 'Alex',
+      last_name: 'Xu',
+      handle: 'alexxu123',
+      image: null,
+      role: 'student',
+    },
+    {
+      id: 'usr_7yA7g65h8sfmuts6',
+      email: 'sam@emial.com',
+      first_name: 'Sam',
+      last_name: 'Smith',
+      handle: 'samsmith',
+      image: null,
+      role: 'tutor',
+    },
+    {
+      id: 'usr_1a2b3c4d5e6f7g8h',
+      email: 'john@email.com',
+      first_name: 'John',
+      last_name: 'Doe',
+      handle: 'johndoe',
+      image: null,
+      role: 'admin',
+    },
+    {
+      id: 'usr_9i8j7k6l5m4n3o2p',
+      email: 'jane@email.com',
+      first_name: 'Jane',
+      last_name: 'Doe',
+      handle: 'janedoe',
+      image: null,
+      role: 'tutor',
+    },
+    {
+      id: 'usr_q1w2e3r4t5y6u7i8',
+      email: 'bob@email.com',
+      first_name: 'Bob',
+      last_name: 'Smith',
+      handle: 'bobsmith',
+      image: null,
+      role: 'student',
+    },
+    {
+      id: 'usr_z1x2c3v4b5n6m7l8',
+      email: 'alice@email.com',
+      first_name: 'Alice',
+      last_name: 'Johnson',
+      handle: 'alicejohnson',
+      image: null,
+      role: 'tutor',
+    },
+  ];
 
   return (
     <main className="grid min-h-[calc(100vh-6.5rem)] flex-1 gap-4 bg-muted/40 p-4 sm:px-6 md:gap-8 lg:grid-cols-3 xl:grid-cols-3 grid-flow-row">
