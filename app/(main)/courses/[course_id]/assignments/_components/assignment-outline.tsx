00a6fbc6666823782a8f31e86bc4a2e3edfca055
Alex <47023455+LexicusRex@users.noreply.github.com>
2024-05-05 10:47:48 +0000

    feat: add outline edit dialog popup for editing outline contents

--- app/(main)/courses/[course_id]/assignments/_components/assignment-outline.tsx
+++ app/(main)/courses/[course_id]/assignments/_components/assignment-outline.tsx
@@ -1,15 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import DeadlineBadge from "@/components/custom/deadline-badge";
+import { Edit, SaveIcon } from "lucide-react";
 import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Skeleton } from "@/components/ui/skeleton";
+import DeadlineBadge from "@/components/custom/deadline-badge";
+
 import Editor from "@/components/editor";
 import { assignmentOutlines } from "../data";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Block } from "@blocknote/core";
+import { toast } from "sonner";
 
 interface AssignmentOutlineProps {
   assignmentId: string;
@@ -75,9 +91,66 @@ export function AssignmentOutline({ assignmentId }: AssignmentOutlineProps) {
         {isLoading ? (
           <Skeleton className="h-8 w-full rounded-lg" />
         ) : (
-          <Editor defaultBlocks={description} showEditToggle />
+          <div>
+            <AssignmentOutlineEditDialog defaultBlocks={description} />
+            <Editor defaultBlocks={description} />
+          </div>
         )}
       </AccordionContent>
     </AccordionItem>
+  );
+}
+
+function AssignmentOutlineEditDialog({
+  defaultBlocks,
+}: {
+  defaultBlocks: Object[];
+}) {
+  const [blocks, setBlocks] = useState<Object[]>([]);
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="ghost" size="sm">
+          <Edit className="mr-2 h-4 w-4" /> Edit
+          <span className="sr-only">Edit Assignment Outline</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-h-[70%] min-w-fit">
+        <DialogHeader>
+          <DialogTitle>Edit Assignment Outline</DialogTitle>
+          <DialogDescription>
+            Edit the details of the assignment outline.
+          </DialogDescription>
+        </DialogHeader>
+        <ScrollArea className="max-h-[500px] overflow-visible">
+          <Editor
+            defaultBlocks={defaultBlocks}
+            editable
+            setContentCallback={setBlocks}
+          />
+        </ScrollArea>
+        <DialogFooter className="">
+          <DialogClose asChild>
+            <Button
+              type="button"
+              onClick={() => {
+                toast(
+                  <div className="w-full">
+                    <p>You submitted the following values:</p>
+                    <pre className="mt-2 max-h-[300px] w-full overflow-x-auto rounded-md bg-slate-900 p-4">
+                      <code className="text-white">
+                        {JSON.stringify(blocks, null, 2)}
+                      </code>
+                    </pre>
+                  </div>,
+                );
+              }}
+            >
+              <SaveIcon className="mr-2 h-4 w-4" /> Save
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
--- app/(main)/courses/[course_id]/assignments/_components/deliverable-outline.tsx
+++ app/(main)/courses/[course_id]/assignments/_components/deliverable-outline.tsx
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { toast } from "sonner";
+import { Edit, Edit3Icon, SaveIcon } from "lucide-react";
 
-import DeadlineBadge from "@/components/custom/deadline-badge";
 import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import DeadlineBadge from "@/components/custom/deadline-badge";
 import Editor from "@/components/editor";
 
 interface DeliverableOutlineProps {
@@ -68,17 +87,88 @@ export function DeliverableOutline({
   }, [deliverableId]);
   return (
     <AccordionItem value={deliverableId}>
-      <AccordionTrigger>
-        {deliverableName}
-        <DeadlineBadge deadline={deadline} className="ml-auto mr-2" />
-      </AccordionTrigger>
+      <div className="group flex items-center justify-between gap-2 overflow-hidden rounded-lg">
+        <AccordionTrigger>
+          {deliverableName}
+          <DeadlineBadge deadline={deadline} className="ml-auto mr-2" />
+        </AccordionTrigger>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="flex w-0 cursor-pointer items-center justify-center transition-all duration-300 group-hover:w-8"
+            >
+              <Edit3Icon className="h-4 w-4 text-muted-foreground" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>Edit deliverable</TooltipContent>
+        </Tooltip>
+      </div>
       <AccordionContent>
         {isLoading ? (
           <Skeleton className="h-8 w-full rounded-lg" />
         ) : (
-          <Editor defaultBlocks={description} showEditToggle />
+          <div>
+            <DeliverableOutlineEditDialog defaultBlocks={description} />
+            <Editor defaultBlocks={description} />
+          </div>
         )}
       </AccordionContent>
     </AccordionItem>
+  );
+}
+
+function DeliverableOutlineEditDialog({
+  defaultBlocks,
+}: {
+  defaultBlocks: Object[];
+}) {
+  const [blocks, setBlocks] = useState<Object[]>([]);
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="ghost" size="sm">
+          <Edit className="mr-2 h-4 w-4" /> Edit
+          <span className="sr-only">Edit Deliverable Outline</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-h-[70%] min-w-fit">
+        <DialogHeader>
+          <DialogTitle>Edit Deliverable Outline</DialogTitle>
+          <DialogDescription>
+            Edit the details of a deliverable outline.
+          </DialogDescription>
+        </DialogHeader>
+        <ScrollArea className="max-h-[500px] overflow-visible">
+          <Editor
+            defaultBlocks={defaultBlocks}
+            editable
+            setContentCallback={setBlocks}
+          />
+        </ScrollArea>
+        <DialogFooter className="">
+          <DialogClose asChild>
+            <Button
+              type="button"
+              onClick={() => {
+                toast(
+                  <div className="w-full">
+                    <p>You submitted the following values:</p>
+                    <pre className="mt-2 max-h-[300px] w-full overflow-x-auto rounded-md bg-slate-900 p-4">
+                      <code className="text-white">
+                        {JSON.stringify(blocks, null, 2)}
+                      </code>
+                    </pre>
+                  </div>,
+                );
+              }}
+            >
+              <SaveIcon className="mr-2 h-4 w-4" /> Save
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
