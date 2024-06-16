a42e57f049cb017a5006c8f52b577b98632cd963
Alex <47023455+LexicusRex@users.noreply.github.com>
2024-06-16 10:43:58 +0000

    fix: add vercel jwt cookie to fetch

--- app/(main)/courses/[course_id]/settings/page.tsx
+++ app/(main)/courses/[course_id]/settings/page.tsx
@@ -1,14 +1,30 @@
-// import { notFound } from "next/navigation";
+import { cookies } from "next/headers";
+import { notFound } from "next/navigation";
+
+import { getCookie } from "cookies-next";
 
 import { Separator } from "@/components/ui/separator";
 
 import { CourseInfoForm } from "./course-info-form";
 
 async function getCourseData(courseId: string) {
-  const response = await fetch(
-    `${process.env.VERCEL_ENV === "development" ? "http" : "https"}://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/courses/${courseId}`,
-    { cache: "no-store" },
-  );
+  const url = `${process.env.VERCEL_ENV === "development" ? "http" : "https"}://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/courses/${courseId}`;
+  console.warn(`URL: ${url}`);
+  const jwtCookie = getCookie("_vercel_jwt", { cookies });
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    cache: "no-store",
+  };
+  options.headers = {
+    ...options.headers,
+    // ...(authToken ? { Authorization: `Bearer ${authToken}` } : {}),
+    ...(jwtCookie ? { Cookie: `_vercel_jwt=${jwtCookie}` } : {}),
+  };
+
+  const response = await fetch(url, options);
   if (!response.ok) {
     return null;
   }
@@ -22,9 +38,9 @@ export default async function SettingsCourseGeneralPage({
 }) {
   const courseData = await getCourseData(params.course_id);
 
-  // if (!courseData) {
-  //   notFound();
-  // }
+  if (!courseData) {
+    notFound();
+  }
 
   return (
     <div className="space-y-6">
