7957d89f5b36863e3bcda8e941980dae56dfb894
Alex <47023455+LexicusRex@users.noreply.github.com>
2024-07-03 11:57:24 +0000

    feat: add datetime picker element for easy combined selection of datetime

---
+++ components/datetime-picker/context.tsx
@@ -0,0 +1,33 @@
+import { createContext, PropsWithChildren, useContext, useState } from "react";
+
+import { setDateByType, TimePickerType } from "./time-picker-utils";
+
+export type TimePickerContextType = {
+  date: Date;
+  setDate: (date: Date) => void;
+};
+
+export const TimePickerContext = createContext<
+  TimePickerContextType | undefined
+>(undefined);
+
+export const TimePickerProvider = ({ children }: PropsWithChildren<{}>) => {
+  const [date, setDate] = useState(new Date());
+  return (
+    <TimePickerContext.Provider value={{ date, setDate }}>
+      {children}
+    </TimePickerContext.Provider>
+  );
+};
+
+export const useTimePickerContext = () => {
+  const context = useContext(TimePickerContext);
+
+  if (!context) {
+    throw new Error(
+      "useTimePickerContext must be used inside the TimePickerProvider",
+    );
+  }
+
+  return context;
+};
---
+++ components/datetime-picker/time-picker-fields.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import * as React from "react";
+
+import { Label } from "@/components/ui/label";
+
+import { MeridiemSelect } from "./meridiem-select";
+import { TimePickerInput } from "./time-picker-input";
+import { Meridiem } from "./time-picker-utils";
+
+interface TimePickerDemoProps {
+  date: Date | undefined;
+  setDate: (date: Date | undefined) => void;
+}
+
+export function TimePickerFields({ date, setDate }: TimePickerDemoProps) {
+  const [meridiem, setMeridiem] = React.useState<Meridiem>("AM");
+
+  const minuteRef = React.useRef<HTMLInputElement>(null);
+  const hourRef = React.useRef<HTMLInputElement>(null);
+  const secondRef = React.useRef<HTMLInputElement>(null);
+  const meridiemRef = React.useRef<HTMLButtonElement>(null);
+
+  return (
+    <div className="flex items-end gap-2">
+      <div className="grid gap-1 text-center">
+        <Label htmlFor="hours" className="text-xs">
+          hrs
+        </Label>
+        <TimePickerInput
+          picker="12hours"
+          meridiem={meridiem}
+          date={date}
+          setDate={setDate}
+          ref={hourRef}
+          onRightFocus={() => minuteRef.current?.focus()}
+        />
+      </div>
+      <div className="grid gap-1 text-center">
+        <Label htmlFor="minutes" className="text-xs">
+          min
+        </Label>
+        <TimePickerInput
+          picker="minutes"
+          id="minutes12"
+          date={date}
+          setDate={setDate}
+          ref={minuteRef}
+          onLeftFocus={() => hourRef.current?.focus()}
+          onRightFocus={() => secondRef.current?.focus()}
+        />
+      </div>
+      {/* <div className="grid gap-1 text-center">
+        <Label htmlFor="seconds" className="text-xs">
+          Seconds
+        </Label>
+        <TimePickerInput
+          picker="seconds"
+          id="seconds12"
+          date={date}
+          setDate={setDate}
+          ref={secondRef}
+          onLeftFocus={() => minuteRef.current?.focus()}
+          onRightFocus={() => meridiemRef.current?.focus()}
+        />
+      </div> */}
+      <div className="grid gap-1 text-center">
+        {/* <Label htmlFor="meridiem" className="text-xs">
+          Meridiem
+        </Label> */}
+        <MeridiemSelect
+          meridiem={meridiem}
+          setMeridiem={setMeridiem}
+          date={date}
+          setDate={setDate}
+          ref={meridiemRef}
+          onLeftFocus={() => secondRef.current?.focus()}
+        />
+      </div>
+    </div>
+  );
+}
