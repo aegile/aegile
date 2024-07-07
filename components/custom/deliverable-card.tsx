07fa0c311b98164adea1a58219a37174435c8623
Alex <47023455+LexicusRex@users.noreply.github.com>
2024-07-07 03:21:31 +0000

    feat: add id to deliverable card for redirecting to specific deliverable page

--- components/custom/deliverable-card.tsx
+++ components/custom/deliverable-card.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import { ArrowRightIcon } from "@radix-ui/react-icons";
 import { FileIcon } from "lucide-react";
 
 import { formatDatetimeFormal } from "@/lib/datetime";
@@ -17,29 +18,29 @@ import {
 import { ClientDateTime } from "./client-datetime";
 
 type DeliverableCardProps = {
-  title: string;
+  id: string;
+  name: string;
   weighting: number;
   deadline: Date;
   description?: string;
   status: string;
   files?: Array<string>;
-  link?: string;
 };
 
 export default function DeliverableCard({
-  title,
+  id,
+  name,
   weighting,
   deadline,
   description = "Submit a working prototype of <project requirement>.",
   status = "Not Submitted",
   files = [],
-  link = "#",
 }: DeliverableCardProps) {
   return (
     <Card className="flex flex-col">
       <CardHeader className="p-4 pb-0">
         <CardTitle className="flex items-center justify-between">
-          <p className="line-clamp-1">{title}</p>
+          <p className="line-clamp-1">{name}</p>
           <Badge variant="success">{status}</Badge>
         </CardTitle>
         <CardDescription className="space-y-2 font-semibold">
@@ -48,22 +49,30 @@ export default function DeliverableCard({
         </CardDescription>
       </CardHeader>
       <CardContent className="flex flex-grow flex-col space-y-2 p-4 pt-4">
-        <div className="line-clamp-2 h-10 text-muted-foreground">
+        <div className="line-clamp-1 text-sm text-muted-foreground">
           {description}
         </div>
         <div className="flex flex-col justify-between">
-          <p className="mt-1 text-sm text-muted-foreground">
+          <p className="mt-1 text-sm text-muted-foreground underline">
             Files ({files.length})
           </p>
         </div>
-        <div className="flex items-center gap-2">
-          <FileIcon className="h-4 w-4" />
-          <span className="text-sm font-semibold">demo-prototype.zip</span>
-        </div>
+        {files?.map((file) => (
+          <div className="flex items-center gap-2">
+            <FileIcon className="h-4 w-4" />
+            <span className="text-sm font-semibold">demo-prototype.zip</span>
+          </div>
+        ))}
       </CardContent>
       <CardFooter className="flex gap-2 p-4 pt-0">
-        <Button size="sm" asChild>
-          <Link href={link}>View Deliverable</Link>
+        <Button
+          size="sm"
+          asChild
+          variant="expandIcon"
+          Icon={ArrowRightIcon}
+          iconPlacement="left"
+        >
+          <Link href={`/deliverables/${id}`}>View</Link>
         </Button>
       </CardFooter>
     </Card>
