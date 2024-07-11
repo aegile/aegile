95ea9bd748e0715f5eaabc0647ee441521b3e4b5
Alex <47023455+LexicusRex@users.noreply.github.com>
2024-07-11 08:23:13 +0000

    chore: remove legacy code for project group mail inbox display

--- app/(main)/classes/[class_id]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ClassHomePage() {
-  return (
-    <main className="grid min-h-[calc(100vh-6.5rem)] flex-1 grid-flow-row gap-4 bg-muted/40 p-4 sm:px-6 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
-      HOME
-    </main>
-  );
-}
